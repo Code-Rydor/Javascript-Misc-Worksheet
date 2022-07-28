dcea67b7de32eac79a6b1a71c565c5380eaf4a49
Garrett <ga.landin89@gmail.com>
2022-07-27 21:08:09 -0500

    More Arrow Functions

--- index.js
+++ index.js
@@ -1059,3 +1059,41 @@ var magic = () => new Date(); // But change it const
 
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+//? Write Higher Order Arrow Functions // -------------------------
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+const squareList = (arr) => {
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num >0).map(x => x * x); // Need to go back over map and filter
+    return squaredIntegers;
+};
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+
+//? Default Parameters //
+
+const increment = (function () {
+    return function increment(number, value = 1) {
+        return number + value;
+    };
+})();
+console.log(increment(5, 2));
+console.log(increment(5));
+
+//? Rest Operator //
+
+// const sum1 = (function() {
+//     return function sum1(x, y, z) {
+//         const args = [x, y, z];
+//         return args.reduce((a, b) => a + b, 0);
+//     };
+// })();
+// console.log(sum1(1, 2, 3));
+
+const sum1 = (function() {
+    return function sum1(...args) { // everything that is passed in with be converted to one array called args
+        return args.reduce((a, b) => a + b, 0);
+    };
+})();
+console.log(sum1(1, 2, 3));
